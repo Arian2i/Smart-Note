ea19e705c40dfa138e8fe510f608772a9e1e43f9
arian2i <aryanazarnioshe@gmail.com>
2025-05-11 08:27:05 +0330

    First test for admin panel

--- notes/admin.py
+++ notes/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Note, Tag
 
-# Register your models here.
+
+@admin.register(Note)
+class NoteAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'user', 'pinned', 'is_archived', 'created_at']
+    list_filter = ['pinned', 'is_archived']
+    search_fields = ['title', 'content']
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name']
