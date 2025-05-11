02f1ea8e8cda4f6539d30d08d56ef443b9e0b573
arian2i <aryanazarnioshe@gmail.com>
2025-05-11 08:15:58 +0330

    create views for notes

--- notes/views.py
+++ notes/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+from rest_framework import generics, permissions
+from .serializers import NoteSerializer
+from .models import Note
 
-# Create your views here.
+
+class NoteCreateView(generics.ListCreateAPIView):
+    serializer_class = NoteSerializer
+    permission_classes = [permissions.IsAuthenticated]
+    
+    
+    def get_queryset(self):
+        return Note.objects.filter(user=self.request.user).order_by('created_at')
+    
+    
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+        
+        
+class NoteDetailView(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = NoteSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+
+    def get_queryset(self):
+        return Note.objects.filter(user=self.request.user)
