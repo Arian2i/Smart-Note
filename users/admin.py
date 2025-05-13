a51f1d03a61d69e95ad1444dfb9b9d98949d974a
Arian Azar <aryanazarnioshe@gmail.com>
2025-05-13 17:18:02 +0330

    create auth

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User
 
-# Register your models here.
+class CustomUserAdmin(UserAdmin):
+    list_display = ('phone', 'first_name', 'last_name', 'gender', 'age', 'is_staff', 'is_superuser')
+    search_fields = ('phone', 'first_name', 'last_name')
+    ordering = ('phone',)
+
+    fieldsets = (
+        (None, {'fields': ('phone', 'password')}),
+        ('Personal Info', {'fields': ('first_name', 'last_name', 'gender', 'age')}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser')}),
+    )
+
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('phone', 'first_name', 'last_name', 'gender', 'age', 'password1', 'password2')
+        }),
+    )
+
+admin.site.register(User, CustomUserAdmin)
